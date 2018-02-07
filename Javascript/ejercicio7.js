f3229e353e7d4674eb0994aefcb1f99870286cc5
curs2018 <curs2018@BI101-13.epsevg.upc.es>
2018-02-07 14:00:58 +0100

    upload 07/02/18

--- Javascript/ejercicio7.js
+++ Javascript/ejercicio7.js
@@ -7,22 +7,30 @@ var diasemana, i;
 
 diasemana = ["lunes", "martes", "miercoles", "jueves", "vienes", "sabado", "domingo"];
 /* 
+//pots posar enlloc de i < 7, i < diasemana.length
 for ( i = 0; i < 7;i++){
 alert(diasemana[i]);
 } 
 */
 /*
+
+//for in
 for (i in diasemana){
     alert(diasemana[i]);
 }
 */
 /*
+
+//while 
 i=0;
 while (i<7){
 alert(diasemana[i])
 i++;
 }
 */
+
+
+//do while
 /*
 i=0;
 do{
@@ -30,7 +38,9 @@ do{
 i++;
 }while(i<7);
 */
-/* 7b */
+
+
+// 7b 
 
 /*
 var final=false;
@@ -46,7 +56,7 @@ while(final!=true){
     var mes=meses[Math.floor((Math.random() * 20) + 1)]
     var posicion = meses.indexOf(mes);
     if(posicion!=-1){
-        alert("ok");
+        alert("el mes " + mes +" se encuentra en la posicion: " + posicion);
         final=true;
     }else{
         alert("error, prova de nou");
@@ -55,6 +65,7 @@ while(final!=true){
 
 
 */ 
+
 
 // 7c 
 /*
@@ -65,15 +76,106 @@ for ( i=1; i <= num ;i++){
 }
 alert(resdo);
 */
- // 7d 
- var inicial = prompt("dame un 2");
 
+
+ // 7d 
+ /*
+ var inicial = prompt("dame un 2");
 
  resdo = inicial;
  while(resdo <100){
    var resdo = resdo* inicial;
  }
  alert(resdo);
- 
+ */
+/* tb es pot fer
+while inicial <= 100){
+    inicial*= inicial;
+}
+*/
 
- 
+
+//7e
+/*
+var i = prompt("dame un numero del 4 al 5");
+for (i; i>0; i--){
+var answer = confirm(i + " " + "¿continuamos?");
+    if (answer == true) {
+        txt = "le has dado a OK!";
+    }   else {
+        txt = "Cancelaste!";
+        break;
+        }
+}
+*/
+/* tb es podria fer
+for
+var respuesta = confirm (i);
+if (!respuesta){
+    alert("La cuenta atrás se interrumpe");
+    break;
+}
+*/
+
+
+//7f
+
+//meses = meses.reverse();
+/*
+i = 0
+for (i; i<= meses.length;i++){
+    alert(meses[i]);
+}
+*/
+// un altre forma de fer-ho sense reverse
+/*
+i = meses.length -1;
+for (i; i >=0;i--){
+    alert(meses[i]);
+}
+*/
+//7g
+
+/* 
+pedir prompt con texto de más de 5 carácteres
+si está vacio o inferior a 5, seguir mostrando prompt
+*/
+do{
+    var quest = prompt("escribe algo de más de 5 carácteres");
+    alert(quest.length);
+    var letrasEspacios = quest.split("");
+    var cadenaSinEspacios = "";
+    for (i in letrasEspacios){
+        if(letrasEspacios[i] != " "){
+            cadenaSinEspacios +=
+            letrasEspacios[i];
+        }
+    }
+    while(texto.length<=5);
+
+/* amb la funcio
+ replace es un altre opció
+}while(quest.length < 5);
+*/
+/*
+var i=0
+while(i<5){
+    var quest = prompt("escribe algo de más de 5 carácteres");
+i = quest.length;
+}
+*/
+
+//7h
+/*
+recorrer string caracter a caracter
+mostrar alert x cada caracter
+al llegar al final se
+*/
+
+texto = prompt("");
+var letras= texto.split("");
+var longitud = letras.length;
+var contador=0;
+while (contador<longitud && letras[contador]!="$"){
+    alert(letras[contadorcontador]++);
+    }
