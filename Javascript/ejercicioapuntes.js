fd85ec21a93e413a54e23da0df4ef5567a8c34b8
curs2018 <curs2018@BI101-13.epsevg.upc.es>
2018-02-14 13:58:40 +0100

    upload 14/02

--- Javascript/ejercicioapuntes.js
+++ Javascript/ejercicioapuntes.js
@@ -2,8 +2,17 @@ window.addEventListener('load', cargaEventos);
 
     function cargaEventos(){
         document.getElementsByClassName("submit").addEventListener("click", alerta);
+        document.getElementById("fotodecoration").addEventListener("click", efectofoto);
     }
 
+    //1 aparezca alert antes de enviar formulario
     function alerta(){
         alert("seguro que quiere enviarlo");
+    }
+    //2 
+    function efectofoto(){
+        img.hover{
+            alert("está prohibida su copia");
+        }
+
     }
